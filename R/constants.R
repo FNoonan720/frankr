284489d6e5832a3b1f019821fd2c41d26e8c3981
FNoonan720 <fnoonan15@gmail.com>
2024-01-09 01:34:42 -0500

    Initial Commit

---
+++ R/constants.R
@@ -0,0 +1,43 @@
+#' nba_com_headers
+nba_com_headers = c(`Accept` = "*/*", `Accept-Language` = "en-US,en;q=0.9", `Connection` = "keep-alive", `Origin` = "https://www.nba.com", `Referer` = "https://www.nba.com/", `Sec-Fetch-Dest` = "empty",
+                    `Sec-Fetch-Mode` = "cors", `Sec-Fetch-Site` = "same-site", `User-Agent` = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+                    `sec-ch-ua` = '"Not_A Brand";v="8", "Chromium";v="120", "Google Chrome";v="120"', `sec-ch-ua-mobile` = "?0", `sec-ch-ua-platform` = '"Windows"')
+
+#' pbp_stats_headers
+pbp_stats_headers = c(`Accept` = "application/json, text/plain, */*", `Accept-Language` = "en-US,en;q=0.9", `Connection` = "keep-alive", `Origin` = "https://www.pbpstats.com",
+                      `Referer` = "https://www.pbpstats.com/", `Sec-Fetch-Dest` = "empty", `Sec-Fetch-Mode` = "cors", `Sec-Fetch-Site` = "same-site",
+                      `User-Agent` = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+                      `sec-ch-ua` = '"Not_A Brand";v="8", "Chromium";v="120", "Google Chrome";v="120"', `sec-ch-ua-mobile` = "?0", `sec-ch-ua-platform` = '"Windows"')
+#' logo_base_url
+logo_base_url <- "https://loodibee.com/wp-content/uploads/"
+
+#' df_teams
+df_teams <- data.frame(
+  ID = c(1610612737, 1610612751, 1610612738, 1610612766, 1610612741, 1610612739, 1610612742, 1610612743, 1610612765, 1610612744,
+         1610612745, 1610612754, 1610612746, 1610612747, 1610612763, 1610612748, 1610612749, 1610612750, 1610612740, 1610612752,
+         1610612760, 1610612753, 1610612755, 1610612756, 1610612757, 1610612758, 1610612759, 1610612761, 1610612762, 1610612764),
+  ABV = c("ATL", "BKN", "BOS", "CHA", "CHI", "CLE", "DAL", "DEN", "DET", "GSW",
+          "HOU", "IND", "LAC", "LAL", "MEM", "MIA", "MIL", "MIN", "NOP", "NYK",
+          "OKC", "ORL", "PHI", "PHX", "POR", "SAC", "SAS", "TOR", "UTA", "WAS"),
+  CTG_NAME = c("Atlanta",       "Brooklyn", "Boston",       "Charlotte",  "Chicago",  "Cleveland",  "Dallas",       "Denver",     "Detroit",      "Golden State",
+               "Houston",       "Indiana",  "LA Clippers",  "LA Lakers",  "Memphis",  "Miami",      "Milwaukee",    "Minnesota",  "New Orleans",  "New York",
+               "Oklahoma City", "Orlando",  "Philadelphia", "Phoenix",    "Portland", "Sacramento", "San Antonio",  "Toronto",    "Utah",         "Washington"),
+  LOGO = c(paste0(logo_base_url,"nba-atlanta-hawks-logo.png"),              paste0(logo_base_url,"brooklyn-nets-logo-symbol.png"),
+           paste0(logo_base_url,"nba-boston-celtics-logo.png"),             paste0(logo_base_url,"charlotte-hornets-logo-symbol.png"),
+           paste0(logo_base_url,"nba-chicago-bulls-logo.png"),              paste0(logo_base_url,"cleveland-cavaliers-logo-symbol.png"),
+           paste0(logo_base_url,"dallas-mavericks-logo-symbol.png"),        paste0(logo_base_url,"denver-nuggets-logo-symbol.png"),
+           paste0(logo_base_url,"nba-detroit-pistons-logo.png"),            paste0(logo_base_url,"nba-golden-state-warriors-logo-2020.png"),
+           paste0(logo_base_url,"houston-rockets-logo-symbol.png"),         paste0(logo_base_url,"indiana-pacers-logo-symbol.png"),
+           paste0(logo_base_url,"los-angeles-clippers-logo-symbol.png"),    paste0(logo_base_url,"nba-los-angeles-lakers-logo.png"),
+           paste0(logo_base_url,"nba-memphis-grizzlies-logo.png"),          paste0(logo_base_url,"miami-heat-logo-symbol.png"),
+           paste0(logo_base_url,"nba-milwaukee-bucks-logo.png"),            paste0(logo_base_url,"minnesota-timberwolves-logo-symbol.png"),
+           paste0(logo_base_url,"new-orleans-pelicans-logo-symbol.png"),    paste0(logo_base_url,"nba-new-york-knicks-logo.png"),
+           paste0(logo_base_url,"oklahoma-city-thunder-logo-symbol.png"),   paste0(logo_base_url,"orlando-magic-logo-symbol.png"),
+           paste0(logo_base_url,"nba-philadelphia-76ers-logo.png"),         paste0(logo_base_url,"phoenix-suns-logo-symbol.png"),
+           paste0(logo_base_url,"portland-trail-blazers-logo-symbol.png"),  paste0(logo_base_url,"nba-sacramento-kings-logo.png"),
+           paste0(logo_base_url,"san-antonio-spurs-logo-symbol.png"),       paste0(logo_base_url,"nba-toronto-raptors-logo-2020.png"),
+           paste0(logo_base_url,"utah-jazz-logo-symbol.png"),               paste0(logo_base_url,"washington-wizards-logo-symbol.png")))
+
+#' theme_colors
+theme_colors <- c(`text` = "gray88", `text_axis` = "gray77", `text_red` = "#FFC9CB", `text_green` = "#BFFFD0", `background_panel` = "gray28", `background_plot` = "gray22",
+                  `grid_lines` = "gray44")
---
+++ R/functions.R
@@ -0,0 +1,121 @@
+#' preview
+#'
+#' @param df df
+#' @param n n
+#'
+#' @export
+preview <- function(df, n) {
+  View(df[1:n,])
+  View(df[(nrow(df)-(n-1)):nrow(df),])
+}
+
+#' get_html_table_from_url
+#'
+#' @param url url
+#' @param idx idx
+#'
+#' @return df
+#' @export
+get_html_table_from_url <- function(url, idx=1) {
+  rvest::read_html(url) %>%
+    rvest::html_nodes("table") %>%
+    rvest::html_table(fill=T) %>%
+    .[[idx]] %>%
+    as.data.frame %>%
+    return
+}
+
+#' get_nba_com_table
+#'
+#' @param url url
+#' @param headers headers
+#' @param params params
+#' @param idx idx
+#'
+#' @return df
+#' @export
+get_nba_com_table <- function(url, headers, params, idx=1) {
+  resultSet <- httr::content(
+    httr::GET(url = url, httr::add_headers(.headers=headers), query = params)) %>%
+    .[['resultSets']] %>%
+    .[[idx]]
+  df <- resultSet %>%
+    .[['rowSet']] %>%
+    data.table::rbindlist %>%
+    as.data.frame %>%
+    setnames(resultSet %>%
+               .[['headers']] %>%
+               unlist)
+  return(df)
+}
+
+#' get_pbp_stats_table
+#'
+#' @param url url
+#' @param headers headers
+#' @param params params
+#' @param single_row single_row
+#'
+#' @return df
+#' @export
+get_pbp_stats_table <- function(url, headers, params, single_row=F) {
+  content <- httr::content(
+    httr::GET(url = url, httr::add_headers(.headers=headers), query = params))
+  if(single_row) {
+    content[['single_row_table_data']] %>%
+      as.data.frame %>%
+      return
+  } else {
+    if(content[['multi_row_table_data']] %>% is.null)
+    {
+      data <- content[['results']]
+    }
+    else {
+      data <- content[['multi_row_table_data']]
+    }
+    data %>%
+      data.table::rbindlist(fill=T) %>%
+      as.data.frame %>%
+      return
+  }
+}
+
+#' get_formula_from_model
+#'
+#' @param model model
+#' @param df_name df_name
+#'
+#' @return formula
+#' @export
+get_formula_from_model <- function(model, df_name) {
+  formula <- model$coefficients["(Intercept)"]
+  for(i in 1:length(model$coefficients[-1])) {
+    formula <- paste0(formula, " + ", model$coefficients[-1][i], " *", df_name,
+                      "$", names(model$coefficients[-1])[i])
+  }
+  return(formula)
+}
+
+#' remaining_sample_avg
+#'
+#' @param entire_weight entire_weight
+#' @param entire_value entire_value
+#' @param sample_weight sample_weight
+#' @param sample_value sample_value
+#'
+#' @return avg
+#' @export
+remaining_sample_avg <- function(entire_weight, entire_value, sample_weight, sample_value) {
+  return((entire_value - sample_weight / entire_weight * sample_value) /
+           ( (entire_weight - sample_weight) / entire_weight) )
+}
+
+#' save_plot
+#'
+#' @param title title
+#'
+#' @return plot
+#' @export
+save_plot <- function(title) {
+  return(ggplot2::ggsave(file = title, units = "in", dpi = 320, width = 10, height = 10))
+}
