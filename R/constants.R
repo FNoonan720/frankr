a0ab6f811f6632167066661ce9c00ca3856a3e91
FNoonan720 <fnoonan15@gmail.com>
2024-01-15 12:19:22 -0500

    Added 'common_headers' for headers that nba.com and pbpstats have in common

--- R/constants.R
+++ R/constants.R
@@ -1,15 +1,17 @@
-#' nba_com_headers
+#' common_headers
 #' @export
-nba_com_headers = c(`Accept` = "*/*", `Accept-Language` = "en-US,en;q=0.9", `Connection` = "keep-alive", `Origin` = "https://www.nba.com", `Referer` = "https://www.nba.com/", `Sec-Fetch-Dest` = "empty",
+common_headers = c(`Accept` = "*/*", `Accept-Language` = "en-US,en;q=0.9", `Connection` = "keep-alive", `Sec-Fetch-Dest` = "empty",
                     `Sec-Fetch-Mode` = "cors", `Sec-Fetch-Site` = "same-site", `User-Agent` = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
                     `sec-ch-ua` = '"Not_A Brand";v="8", "Chromium";v="120", "Google Chrome";v="120"', `sec-ch-ua-mobile` = "?0", `sec-ch-ua-platform` = '"Windows"')
 
+#' nba_com_headers
+#' @export
+nba_com_headers = c(common_headers, `Origin` = "https://www.nba.com", `Referer` = "https://www.nba.com/")
+
 #' pbp_stats_headers
 #' @export
-pbp_stats_headers = c(`Accept` = "application/json, text/plain, */*", `Accept-Language` = "en-US,en;q=0.9", `Connection` = "keep-alive", `Origin` = "https://www.pbpstats.com",
-                      `Referer` = "https://www.pbpstats.com/", `Sec-Fetch-Dest` = "empty", `Sec-Fetch-Mode` = "cors", `Sec-Fetch-Site` = "same-site",
-                      `User-Agent` = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
-                      `sec-ch-ua` = '"Not_A Brand";v="8", "Chromium";v="120", "Google Chrome";v="120"', `sec-ch-ua-mobile` = "?0", `sec-ch-ua-platform` = '"Windows"')
+pbp_stats_headers = c(common_headers, `Origin` = "https://www.pbpstats.com", `Referer` = "https://www.pbpstats.com/")
+
 #' logo_base_url
 #' @export
 logo_base_url <- "https://loodibee.com/wp-content/uploads/"
