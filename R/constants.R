0d423944cb7423cef821ba95fbffd151e8197446
Frank Noonan <fnoonan15@gmail.com>
2024-10-31 14:11:30 -0400

    Added Custom Color column in df_teams

--- R/constants.R
+++ R/constants.R
@@ -74,7 +74,10 @@ COLOR_PRIMARY =    c("#E03A3E","#000000","#007A33","#00788C","#CE1141","#860038"
                      "#007AC1","#0077C0","#006BB6","#E56020","#E03A3E","#5A2D81","#C4CED4","#CE1141","#002B5C","#002B5C"),
 COLOR_SECONDARY =  c("#26282A","#FFFFFF","#000000","#1D1160","#FFFFFF","#FDBB30","#00538C","#0E2240","#C8102E","#1D428A",
                      "#000000","#002D62","#C8102E","#FDB927","#12173F","#F9A01B","#EEE1C6","#236192","#C8102E","#006BB6",
-                     "#EF3B24","#C4CED4","#ED174C","#1D1160","#000000","#63727A","#000000","#000000","#00471B","#E31837"))
+                     "#EF3B24","#C4CED4","#ED174C","#1D1160","#000000","#63727A","#000000","#000000","#00471B","#E31837"),
+COLOR_CUSTOM =     c("#E03A3E","#000000","#007A33","#00788C","#CE1141","#B3004A","#002B5E","#FEC524","#1D42BA","#FFC72C",
+                     "#CE1141","#FDBB30","#1D428A","#552583","#5D76A9","#9A002A","#00471B","#0C2340","#85714D","#F58426",
+                     "#007AC1","#0077C0","#006BB6","#E56020","#E03A3E","#5A2D81","#C4CED4","#CE1141","#002B5C","#002B5C"))
 df_teams$CONF <- sapply(df_teams$ABV, function(ABV) { ifelse(is.element(ABV, eastern_conf), "E", "W") })
 df_teams$FULL_NAME <- paste0(df_teams$CITY, " ", df_teams$NAME)
 df_teams$FULL_NAME[df_teams$ABV == "LAC"] <- "LA Clippers"
